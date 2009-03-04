5e0936b99e2d50769c5432b47c4d07c3dcdd37c6
The Android Open Source Project <initial-contribution@android.com>
2009-03-03 19:30:07 -0800

    auto import from //depot/cupcake/@135843

---
+++ Android.mk
@@ -0,0 +1,20 @@
+ifneq ($(TARGET_SIMULATOR),true)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=\
+	netcat.c
+
+LOCAL_CFLAGS:=-O2 -g
+#LOCAL_CFLAGS+=-DLINUX
+
+LOCAL_MODULE_TAGS:=tests eng
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+
+LOCAL_MODULE:=nc
+
+include $(BUILD_EXECUTABLE)
+
+endif  # TARGET_SIMULATOR != true
